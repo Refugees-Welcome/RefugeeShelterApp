0e7a69d41c28ecf6c2ea646cbb62004b725a4109
Rospini <mulatsch@msn.com>
2022-03-08 16:52:38 +0100

    adding unique key and useeffect of userprofile got edit

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -8,22 +8,26 @@ export default function UserProfile(props) {
   const { user } = useContext(AuthContext);
 
   const [authorShelters, setAuthorShelters] = useState([])
-
+  
   useEffect(() => {
-    const userFilter = props.shelter.filter(shelter => shelter.author._id === user._id)
-        
-          return setAuthorShelters(userFilter)
-        
-  }, [])
-  if (user === null) {
+    if(user){
+          const userFilter = props.shelter.filter(
+            (shelter) => shelter.author._id === user._id
+          );
+
+          return setAuthorShelters(userFilter);
+    } 
+  }, [user])
+  
+if (user === null) {
     return (<div>loading…</div>);
   }
 
   return (
     <div className="UserProfile">
-      <h1> User Profile from {user.username}</h1>
+      <h1 className="userProfile"> User Profile from {user.username}</h1>
       {authorShelters.length === 0?<div>currently no created shelter</div>:
-    authorShelters.map(shelter => {return (<div><Link to={`/shelterEdit/${shelter._id}`}>edit: {shelter.name}</Link></div>)
+    authorShelters.map(shelter => {return (<div key={shelter._id} className="CreatedShelters"><Link className="linkCreatedShelters" to={`/shelterEdit/${shelter._id}`}>edit: {shelter.name}</Link></div>)
       })}
     </div>
   )
