2a5eea1180ffaea82749e44bc0e595c2fb508e4a
Rospini <mulatsch@msn.com>
2022-03-11 12:40:51 +0100

    some little mistakes found and solved

--- src/components/ShelterDetails.js
+++ src/components/ShelterDetails.js
@@ -27,28 +27,28 @@ export default function ShelterDetails() {
                     <div className="col-md-6 rounded border border-warning mapped">
                         <div className="form-group">
                             <h1 className="Name text-start">Name: <br></br></h1>
-                            <h1 className="text-end mb-4">{details.name}</h1>
+                            <h1 className="text-start mb-4">{details.name}</h1>
 
                         </div>
                         <br></br>
                         <div className="form-group ">
                             <h4 className="Languages text-start">Languages:<br></br> </h4>
-<h4 className="text-end mb-4">{details.languages}</h4>
+<h4 className="text-start mb-4">{details.languages}</h4>
                         </div>
                         <br></br>
                         <div className="form-group">
                             <h4 className="ContactInfo text-start">ContactInfo:<br></br> </h4>
-<h4 className="text-end mb-4">{details.contactInfo}</h4>
+<h4 className="text-start mb-4">{details.contactInfo}</h4>
                         </div>
                         <br></br>
                         <div className="form-group ">
                             <h4 className="Description text-start">Description: <br></br></h4>
-<h4 className="text-end mb-4">{details.description}</h4>
+<h4 className="text-start mb-4">{details.description}</h4>
                         </div>
                         <br></br>
                         <div className="form-group ">
                             <h4 className="Address text-start">Address:<br></br></h4>
-<h4 className="text-end mb-4">{details.address}</h4>
+<h4 className="text-start mb-4">{details.address}</h4>
                         </div>
                         <br></br>
                         <div className="col-6">
